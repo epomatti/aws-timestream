41a0443680464b05ecb83476a277a5d91726515a
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-07 22:25:26 -0300

    timestream

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,4 +1,50 @@
 import 'dotenv/config'
-import { TimestreamWriteClient, CreateDatabaseCommand } from "@aws-sdk/client-timestream-write";
+import {
+  TimestreamWriteClient, TimestreamWriteClientConfig, WriteRecordsCommand, WriteRecordsCommandInput, _Record
+} from "@aws-sdk/client-timestream-write";
 
-// const { TimestreamWriteClient, CreateDatabaseCommand } = require("@aws-sdk/client-timestream-write");
+(async () => {
+
+  const configuration: TimestreamWriteClientConfig = {
+
+  }
+
+  const writeClient = new TimestreamWriteClient(configuration);
+
+  const currentTime = Date.now().toString(); // Unix time in milliseconds
+
+  const dimensions = [
+    { 'Name': 'region', 'Value': 'us-east-1' },
+    { 'Name': 'az', 'Value': 'az1' },
+    { 'Name': 'hostname', 'Value': 'host1' }
+  ];
+
+  const cpuUtilization = {
+    'Dimensions': dimensions,
+    'MeasureName': 'cpu_utilization',
+    'MeasureValue': '13.5',
+    'MeasureValueType': 'DOUBLE',
+    'Time': currentTime.toString()
+  };
+
+  const memoryUtilization: _Record = {
+    'Dimensions': dimensions,
+    'MeasureName': 'memory_utilization',
+    'MeasureValue': '40',
+    'MeasureValueType': 'DOUBLE',
+    'Time': currentTime.toString()
+  };
+
+  const records = [cpuUtilization, memoryUtilization];
+
+  const input: WriteRecordsCommandInput = {
+    DatabaseName: "timestream-sandbox",
+    TableName: "orders",
+    Records: records
+  }
+
+  const command = new WriteRecordsCommand(input);
+
+  await writeClient.send(command);
+
+})();
