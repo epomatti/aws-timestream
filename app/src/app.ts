c0e2975d6a2f517d8df4bc302855045c4124d058
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-07 22:02:43 -0300

    setup

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,1 +1,4 @@
 import 'dotenv/config'
+import { TimestreamWriteClient, CreateDatabaseCommand } from "@aws-sdk/client-timestream-write";
+
+// const { TimestreamWriteClient, CreateDatabaseCommand } = require("@aws-sdk/client-timestream-write");
